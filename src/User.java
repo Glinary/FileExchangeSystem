3921936c628e376c488fccfd9a5868f4c5ae85f2
glinary <glarclone@gmail.com>
2023-11-23 03:56:41 +0800

    added User, added output feedbacks

---
+++ src/User.java
@@ -0,0 +1,30 @@
+package FileExchangeSystem.src;
+
+public class User {
+    private String alias;
+
+    public User(String alias) {
+        this.alias = alias;
+    }
+
+    private void disconnect() {
+
+    }
+
+    private void sendFile() {
+
+    }
+
+    private void getReceiptAfterSend() {
+
+    }
+
+    private void getReceiptAfterReceive() {
+
+    }
+
+    private void getTimeStamp() {
+
+    }
+
+}
