d4495d0b9c4209d9c34d0947bb3706c8b703db77
glinary <glarclone@gmail.com>
2023-12-05 23:09:27 +0800

    added display feedback for command not found

--- FileExchange/Client/Controller.java
+++ FileExchange/Client/Controller.java
@@ -348,6 +348,7 @@ public class Controller implements ActionListener, DocumentListener, MessageCall
                 
 
             } else {
+                lastCmdDisplay();
                 gui.clientTerminalOut("Error: Command not found.");
                 gui.setUserInput("");
             }
